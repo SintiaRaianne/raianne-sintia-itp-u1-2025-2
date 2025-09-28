3a530e08b9074c3357327c821031d1cbf697a7f0
SintiaRaianne <112667776+SintiaRaianne@users.noreply.github.com>
2025-09-28 02:30:43 -0300

    Create problema03.c
    Pousando a sonda espacial

---
+++ listas/semana05-funcoes/problema03.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <math.h>
+
+// Calcula a altitude no ponto x, y
+float calcula_altura(float x, float y){
+    return sin(cos(y) + x) + cos(y + sin(x));
+}
+
+int seguro(float x, float y){
+    float altura = calcula_altura(x, y);
+
+    if (altura < 0.0 || altura > 2.0){
+        return 0;
+    }
+
+    // Verifica se os 4 pontos verdes sao seguros 
+    float px[4] = {0.2, -0.2, 0.2, -0.2};
+    float py[4] = {0.2, -0.2, -0.2, 0.2};
+    
+    for (int i = 0; i < 4; i++) {
+        altura = calcula_altura(x + px[i], y + py[i]);
+        if (altura < 0.0 || altura > 2.0) { 
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int main() {
+    float x, y;
+
+    scanf("%f %f", &x, &y);
+
+    if (!seguro(x, y)){
+        printf("Troque as coordenadas.");
+        return 0;
+    }
+
+    // Verifica pontos vizinhos
+    float pxn[4] = {2, -2, 0, 0};
+    float pyn[4] = {0, 0, -2, 2};
+    
+    int count = 0;
+    
+    for (int i = 0; i < 4; i++) {
+        if (seguro(x + pxn[i], y + pyn[i])) {
+             count++;
+        }
+    }
+    
+    if (count == 4){
+        printf("seguro\n");
+    } else if (count >= 2) {
+        printf("relativamente seguro\n");
+    } else {
+        printf("inseguro\n");
+    }
+    return 0;
+}
