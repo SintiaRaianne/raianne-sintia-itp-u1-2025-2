01978f914e2835cbe3fc49d5cbd13ad5ef4cff99
SintiaRaianne <112667776+SintiaRaianne@users.noreply.github.com>
2025-09-28 02:35:03 -0300

    Create problema08.c
    Compra de placas de alumínio

---
+++ listas/semana04-repeticoes-b/problema08.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+
+int main(){
+
+    float preco_fornecedor1, preco_fornecedor2;
+    float quantia_disponivel;
+    float custo, resto;
+    
+
+    printf("\nDigite a quantia disponivel para compra: R$ ");
+    scanf("%f", &quantia_disponivel);
+
+    printf("Digite o preco do primeiro fornecedor: R$ ");
+    scanf("%f", &preco_fornecedor1);
+
+    printf("Digite o preco do segundo fornecedor: R$ ");
+    scanf("%f", &preco_fornecedor2);
+
+    for (int i = 0; i <= 10; i++){
+        for (int j = 0; j <= 10; j++){
+
+            custo = (i * preco_fornecedor1 + j * preco_fornecedor2);
+
+            if (quantia_disponivel >= custo){
+                resto = quantia_disponivel - custo;
+
+                printf("Comprando %i do primeiro e %i do segundo resta: %f \n", i, j, resto);
+
+            }
+        }
+    }
+
+    return 0;
+
+}
