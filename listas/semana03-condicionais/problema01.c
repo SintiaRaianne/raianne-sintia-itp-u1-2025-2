033ddd18571df46e30b0b7404030d4af4d312d29
SintiaRaianne <112667776+SintiaRaianne@users.noreply.github.com>
2025-09-28 02:19:34 -0300

    Create problema01.c
    Classificação de IMC

---
+++ listas/semana03-condicionais/problema01.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+
+int main() {
+
+    float peso, altura, massa_corporal;
+
+    printf("\nDigite o peso (kg): ");
+    scanf("%f", &peso);
+
+    printf("Digite a altura (m): ");
+    scanf("%f", &altura);
+
+    massa_corporal = ((peso) / (pow(altura,2)));
+
+    printf("\nIndice de massa corporal: %.2f\n", massa_corporal);
+
+    // Classifique os resultado de acordo com a tabela
+
+    if (massa_corporal < 18.5){
+        printf("Classificacao: Abaixo do peso");
+    } else if (massa_corporal <= 24.9){
+        printf("Classificacao: Peso normal");
+    } else if (massa_corporal <= 29.9){
+        printf("Classificacao: Sobrepeso");
+    } else{
+        printf("Classificacao: Obesidade");
+    }
+    
+    return 0;
+}
