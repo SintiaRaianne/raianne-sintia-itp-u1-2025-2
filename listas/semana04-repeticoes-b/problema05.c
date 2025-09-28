daf5cc450636e1168a708cfe88f1aee53799b4b8
SintiaRaianne <112667776+SintiaRaianne@users.noreply.github.com>
2025-09-28 02:34:02 -0300

    Create problema05.c
    Padrão numérico triangular

---
+++ listas/semana04-repeticoes-b/problema05.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+
+    int n, cont = 1;
+
+    printf("\nDigite o numero de linhas: ");
+    scanf("%i", &n);
+
+    // Caso o usuario informe um numero invalido
+
+    if (n <= 0) {
+        printf("\nPor favor, insira um numero positivo");
+
+        // Encerra o programa com um código de erro e mensagem de aviso.
+        return 1;
+    }
+
+    for (int i = 1; i <= n; i++){
+        for (int j = 1; j <= i; j++){
+            printf("%i ", cont);
+            cont++;
+        }
+        printf("\n");
+    }
+
+    return 0;
+
+}
