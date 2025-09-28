4e3583b374f648017ebb216842a86e9ff4eef23e
SintiaRaianne <112667776+SintiaRaianne@users.noreply.github.com>
2025-09-28 02:20:11 -0300

    Create problema02.c
    Calculadora de energia elétrica

---
+++ listas/semana03-condicionais/problema02.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+
+    // toupper padroniza para maiúsculo
+
+    float consumo, valor_total_conta;
+    char tipo_consumidor;
+    int taxa_fixa = 15;
+    
+    printf("\nDigite o consumo de energia em (kWh): ");
+    scanf("%f", &consumo);
+
+    printf("Digite o tipo de consumidor (R: residencial, C: comercial e I: industrial): ");
+    scanf(" %c", &tipo_consumidor);
+    tipo_consumidor = toupper(tipo_consumidor);
+
+    // Calcula o valor total da conta
+
+    if (tipo_consumidor == 'R'){
+        valor_total_conta = (0.60 * consumo)+ (taxa_fixa);
+    } else if (tipo_consumidor == 'C') {
+        valor_total_conta = (0.48 * consumo) + (taxa_fixa);
+    } else {
+        valor_total_conta = (1.29 * consumo) + (taxa_fixa);
+    }
+    
+    printf("\nValor total da conta R$: %.2f\n", valor_total_conta);
+    
+    return 0;
+}
