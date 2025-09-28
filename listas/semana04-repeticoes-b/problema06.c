1d3015bdd87c1985da55c4f564a477392a2ba76c
SintiaRaianne <112667776+SintiaRaianne@users.noreply.github.com>
2025-09-28 02:34:20 -0300

    Create problema06.c
    Trios pitagóricos

---
+++ listas/semana04-repeticoes-b/problema06.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h> 
+
+int main() {
+
+    int a, b, c;     
+
+    printf("\nTrios pitagoricos com o maior numero menor que 1000: \n");
+
+    for (a = 1; a < 1000; a++) {
+        for (b = a; b < 1000; b++) {
+
+            c = (int)sqrt((pow(a, 2) + pow(b, 2)));
+            
+            if (c < 1000 && (pow(c, 2) == pow(a, 2) + pow(b, 2))) {
+                printf("%i %i %i \n", a, b, c);
+            }
+        }
+    }
+
+    return 0;
+}
