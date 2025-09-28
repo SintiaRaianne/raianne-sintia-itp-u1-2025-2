9b977f25a0cdddf2f98a6b10f2afcd3af9aa152e
SintiaRaianne <112667776+SintiaRaianne@users.noreply.github.com>
2025-09-28 02:16:37 -0300

    Create problema03.c
    Cálculo de juros compostos

---
+++ listas/semana02-variaveis/problema03.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+
+    float capital_inicial;
+    float juros;
+    int tempo;
+    float montante;
+
+    printf("\nDigite o valor do capintal inicial: R$ ");
+    scanf("%f", &capital_inicial);
+
+    printf("Digite a taxa de juros (em porcentagem): ");
+    scanf("%f", &juros);
+
+    printf("Digite o tempo (em anos): ");
+    scanf("%i", &tempo);
+
+    montante = (capital_inicial * pow((1 + (juros/100)), tempo));
+
+    printf("Montante: R$ %.2f\n", montante);
+
+    return 0;
+}
