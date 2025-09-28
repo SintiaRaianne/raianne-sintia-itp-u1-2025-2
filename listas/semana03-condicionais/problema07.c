19e214693c38fce37aa8d00694ba75751a737080
SintiaRaianne <112667776+SintiaRaianne@users.noreply.github.com>
2025-09-28 02:23:29 -0300

    Create problema07.c
    Sistema de equações do 2º grau

---
+++ listas/semana03-condicionais/problema07.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <math.h>
+
+
+int main() {
+
+    double a,b,c, delta, x1, x2;
+
+    printf("\nDigite o valor do coeficiente a: ");
+    scanf("%lf", &a);
+
+    printf("Digite o valor do coeficiente b: ");
+    scanf("%lf", &b);
+
+    printf("Digite o valor do coeficiente c: ");
+    scanf("%lf", &c);
+
+    // Verificando se é uma equação de segundo grau
+
+    if (a != 0){
+
+        // Calculando o delta
+
+        delta = ((pow(b,2)) - (4 * a * c));
+
+        // Determinando as raizes de acordo com delta
+
+            if (delta > 0){
+                x1 = ((-b + (sqrt(delta))) / (2 * a));
+                x2 = ((-b - (sqrt(delta))) / (2 * a));
+                printf("\nDuas raizes reais distintas: x1 = %.2f e x2 = %.2f", x1, x2);
+            } else if (delta == 0){
+                x1 = ((-b + (sqrt(delta))) / (2 * a));
+                printf("\nUma raiz real x1 = x2 = %.2f", x1);
+            } else{
+                printf("\nNao possui raizes reais.");
+            }  
+
+    } else{
+        printf("\nNao e uma equacao de segundo grau.");
+    }
+    
+    return 0;
+
+}
