eeb6fdad4374be1fbd9e17d3afe718d7ff9c403f
SintiaRaianne <112667776+SintiaRaianne@users.noreply.github.com>
2025-09-30 21:45:52 -0300

    Números primos em intervalo

--- listas/semana04-repeticoes-b/problema07.c
+++ listas/semana04-repeticoes-b/problema07.c
@@ -14,22 +14,20 @@ int main(){
     } while (a <= b);
 
     printf("\nNumeros primos entre %i e %i: ", b, a);
-
+    
+    // Encontra e imprime todos os números primos entre a e b
     for (int i = b + 1; i < a; i++){
-
         int primo = 1;
 
         if (i < 2){
             primo = 0;
         }
-
         for (int j = 2; j * j <= i; j++){
             if (i % j == 0){
                 primo = 0;
                 break;
             }
         }
-
         if (primo == 1){
             printf("%i ", i);
         }
