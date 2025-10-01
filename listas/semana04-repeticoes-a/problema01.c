6c866251247b9655109e8a186a8f268eedf21a21
SintiaRaianne <112667776+SintiaRaianne@users.noreply.github.com>
2025-09-30 21:39:49 -0300

    Dobrando até não poder mais

--- listas/semana04-repeticoes-a/problema01.c
+++ listas/semana04-repeticoes-a/problema01.c
@@ -14,16 +14,13 @@ int main() {
     printf("Digite o comprimento do bolso: ");
     scanf("%f", &comprimento_bolso);
 
-    // Enquanto os dois lados forem maiores que o bolso
-
+    // Enquanto os dois lados forem maiores que o bolso, dobra o maior lado
     while (comprimento_folha > comprimento_bolso && largura > comprimento_bolso){
 
-        // Quantidade de dobras realizadas
-
+        // Quantidade de dobras realizadas 
         dobras = dobras + 1;
 
-        // Dobramos o maior lado
-        
+        // Dobra o maior lado 
         if(comprimento_folha > largura){
             comprimento_folha = (comprimento_folha / 2);
         } else {
