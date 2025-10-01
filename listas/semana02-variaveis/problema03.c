bba3941e537539da692555335631d3c710754f28
SintiaRaianne <112667776+SintiaRaianne@users.noreply.github.com>
2025-09-30 21:32:18 -0300

    Cálculo de juros compostos

--- listas/semana02-variaveis/problema03.c
+++ listas/semana02-variaveis/problema03.c
@@ -17,6 +17,7 @@ int main() {
     printf("Digite o tempo (em anos): ");
     scanf("%i", &tempo);
 
+    // Calcular o montante com os dados fornecidos
     montante = (capital_inicial * pow((1 + (juros/100)), tempo));
 
     printf("Montante: R$ %.2f\n", montante);
